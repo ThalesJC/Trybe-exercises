6bfe5f76b702d62c7668c8770a9022418d8cf345
ThalesJC <thales.soul96@gmail.com>
2022-05-08 22:18:29 -0300

    [exercicios 1 e 2]Completos

--- Front-End/exercicios/bloco-10-dia-1/src/index.js
+++ Front-End/exercicios/bloco-10-dia-1/src/index.js
@@ -1,12 +1,9 @@
+/** lista de imports da pagina */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/**render do elemento root */
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);
